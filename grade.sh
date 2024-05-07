98b0020d5542aeb5963a3e317e1b07b62554174b
Rebecca Tseng <165713650+retseng@users.noreply.github.com>
2024-05-07 19:49:05 +0000

    Pending changes exported from your codespace

--- grade.sh
+++ grade.sh
@@ -8,9 +8,25 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "ListExamples.java file found"
+else
+    echo "ListExamples.java file not found."
+    echo "Grade: 0"
+    exit
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+echo $CPATH
+javac -cp $CPATH TestListExamples.java ListExamples.java
+
+echo "The exit code for the compile step is $?."
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
